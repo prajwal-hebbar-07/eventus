9c434cb5154665ae922d9eaafc9a4e148c593f5f
Hebbar <prajwalhebbaras@gmail.com>
2024-09-03 17:23:49 +0530

    feat(events): create EventItem component and integrate with EventsList
        - Created `EventItem` component to display individual event details in a card format.
        - Added a redirect button in the `EventItem` component to navigate to the Event Details page.
        - Integrated the `EventItem` component into the `EventsList` component to render each event as a card.
        - Ensured that each event in the `EventsList` is displayed with all relevant details and a working redirect button.

--- src/components/events/event-item.js
+++ src/components/events/event-item.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function EventItem({ id, title, location, date, image }) {
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
     day: 'numeric',
@@ -7,13 +9,13 @@ export default function EventItem({ id, title, location, date, image }) {
   const formattedAddress = location.replace(', ', '\n')
 
   return (
-    <li className='list-none rounded-lg bg-white my-4 flex flex-row w-[48rem]'>
+    <li className='list-none rounded-lg bg-white my-4 flex flex-row w-[42rem]'>
       <img
         src={`/${image}`}
         alt={title}
         className='h-52 w-auto rounded-l-lg object-contain'
       />
-      <div className='mx-4 my-2'>
+      <div className='mx-4 my-auto w-full'>
         <h2 className='text-2xl font-bold text-zinc-800 mb-2'>{title}</h2>
         <time className='text-base my-2 text-neutral-500'>
           {humanReadableDate}
@@ -23,6 +25,13 @@ export default function EventItem({ id, title, location, date, image }) {
           className='text-xs my-1 text-neutral-500'>
           {formattedAddress}
         </address>
+        <div className='mt-4 float-right'>
+          <Link
+            href={`/events/${id}`}
+            className='py-2 px-4 bg-emerald-500 rounded-lg text-zinc-200 font-medium'>
+            Explore Event
+          </Link>
+        </div>
       </div>
     </li>
   )

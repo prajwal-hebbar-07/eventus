a97b9470a3e6a7afbc2f22b18df672ea95b28d02
Hebbar <prajwalhebbaras@gmail.com>
2024-09-05 11:40:03 +0530

    feat(event-details): fetch event details based on event_id from URL
        - Used the `useRouter` hook from `next/router` to capture the `event_id` param from the URL.
        - Called the `getEventById` function from `events.js`, passing the captured `event_id` to fetch the event details.
        - Handled the case where the event is not found by displaying an appropriate error message to the user.

--- src/pages/events/[event_id].js
+++ src/pages/events/[event_id].js
@@ -1,4 +1,22 @@
+import { useRouter } from 'next/router'
+
+import { getEventById } from '@/data/events'
+
 export default function EventDetailsPage() {
+  const router = useRouter()
+  const id = router.query.event_id
+  const event = getEventById(id)
+
+  if (!event) {
+    return (
+      <div className='md:container mx-auto h-screen flex flex-col justify-center items-center'>
+        <p className='text-center text-5xl font-bold text-red-600'>
+          No Event Found!
+        </p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Event Details Page</h1>

b2a29284f69296f4f724369a26fccb2841249d68
Hebbar <prajwalhebbaras@gmail.com>
2024-09-06 12:58:23 +0530

    feat(event-details): design and style Event Details page
        - Completed the layout and design of the Event Details page to display individual event details.
        - Displayed event-specific information such as title, date, location, and image.
        - Styled the Event Details page using Tailwind CSS for a polished and responsive design.
        - Ensured the page is visually appealing and consistent with the overall project styling.

--- src/pages/events/[event_id].js
+++ src/pages/events/[event_id].js
@@ -7,6 +7,13 @@ export default function EventDetailsPage() {
   const id = router.query.event_id
   const event = getEventById(id)
 
+  const humanReadableDate = new Date(event?.date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  })
+  const formattedAddress = event?.location.replace(', ', '\n')
+
   if (!event) {
     return (
       <div className='md:container mx-auto h-screen flex flex-col justify-center items-center'>
@@ -18,8 +25,28 @@ export default function EventDetailsPage() {
   }
 
   return (
-    <div>
-      <h1>Event Details Page</h1>
+    <div className='md:container mx-auto h-screen flex flex-col justify-center items-center'>
+      <h1 className='text-6xl text-sky-700 mb-8 drop-shadow-lg font-bold'>
+        {event.title}
+      </h1>
+      <div className='mt-4 w-[42rem] p-12 rounded-xl h-[20rem] bg-[#dda15e] flex flex-row justify-around items-center'>
+        <img
+          className='rounded-full w-[16rem] h-[16rem]'
+          src={`/${event.image}`}
+          alt={event.title}
+        />
+        <div>
+          <time className='my-4 text-2xl font-bold text-zinc-900'>
+            {humanReadableDate}
+          </time>
+          <address className='my-4 text-2xl font-bold whitespace-pre-wrap text-slate-600'>
+            {formattedAddress}
+          </address>
+        </div>
+      </div>
+      <p className='my-8 text-3xl font-medium text-neutral-500 w-8/12 tracking-wide leading-relaxed text-center'>
+        {event.description}
+      </p>
     </div>
   )
 }
